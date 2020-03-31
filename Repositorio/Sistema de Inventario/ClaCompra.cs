89d835964d8c0e585217f7a9faefe2992782b0ec
Linda Discua <lindadiscua1998@gmail.com>
2020-03-30 20:19:30 -0600

    se actualizo

---
+++ Repositorio/Sistema de Inventario/ClaCompra.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Inventario
+{
+    class ClaCompra
+    {
+        //----- Encabezado Compra
+        private string numeroFactura;
+        private int proveedor;
+        private DateTime fecha;
+        ////-------Detalle Compra
+        private int idDetalleCompra;
+        private string encabezadoCompra;
+        private string producto;
+        private int cantidad;
+        private decimal precio;
+        private decimal impuesto;
+        private decimal subTotal;
+        private decimal total;
+
+        private ClaConexion conexion;
+
+        public ClaCompra()
+        {
+            numeroFactura = "0000";
+            proveedor = 0;
+            fecha = DateTime.Today;
+
+            idDetalleCompra = 0;
+            encabezadoCompra = "0000";
+            producto = "0000";
+            cantidad = 0;
+            precio = 0;
+            impuesto = 0;
+            subTotal = 0;
+            total = 0;
+        }
+
+        public ClaCompra(string noFac, int pro, DateTime fe, int idDetalle, string encaCompra, string produc, int can,
+            decimal pre, decimal imp, decimal subt, decimal tot)
+        {
+            numeroFactura = noFac;
+            proveedor = pro;
+            fecha = fe;
+
+            idDetalleCompra = idDetalle;
+            encabezadoCompra = encaCompra;
+            producto = produc;
+            cantidad = can;
+            precio = pre;
+            impuesto = imp;
+            subTotal = subt;
+            total = tot;
+        }
+
+    
+        public string NumeroFactura
+        {
+            get{ return numeroFactura; }
+
+            set{ value = numeroFactura;}
+        }
+        
+        public int Proveedor 
+        {
+            get { return proveedor; }
+            set { value = proveedor; }
+        }
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { value = fecha; }
+        }
+
+        ///----------------
+        public int IdDetalleCompra
+        {
+            get { return idDetalleCompra; }
+            set { value = idDetalleCompra; }
+        }
+
+        public string EncabezadoCompra
+        {
+            get { return encabezadoCompra; }
+            set { value = encabezadoCompra; }
+        }
+
+        public string Producto
+        {
+            get { return producto; }
+            set { value = producto; }
+        }
+
+        public int Cantidad
+        {
+            get { return cantidad; }
+            set { value = cantidad; }
+        }
+
+        public decimal Precio
+        {
+            get { return precio; }
+            set { value = precio; }
+        }
+
+        public decimal Impuesto
+        {
+            get { return impuesto; }
+            set { value = impuesto; }
+        }
+
+        public decimal SubTotal
+        {
+            get { return subTotal; }
+            set { value = subTotal; }
+        }
+
+        public decimal Total
+        {
+            get { return total; }
+            set { value = total; }
+        }
+
+    }
+}
---
+++ Repositorio/Sistema de Inventario/ClaConexion.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Sistema_de_Inventario
+{
+    class ClaConexion
+    {
+        private MySqlConnection conexion;
+        private string bd;
+        private string servidor;
+        private string usuario;
+        private string pass;
+
+        public ClaConexion() 
+        {
+            bd = "BDTaller";
+            servidor = "127.0.0.1";
+            usuario = "Linda Discua";
+            pass = "Hello1998";
+            conexion = new MySqlConnection();
+        }
+
+        public ClaConexion(string b, string ser, string us, string p)
+        {
+            bd = b;
+            servidor = ser;
+            usuario = us;
+            pass = p;
+            conexion = new MySqlConnection();
+        }
+
+        public void conectar()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Closed)
+                {
+                    conexion.ConnectionString = string.Format("Database={0};Server={1};Uid={2};Pwd={3};SslMode=none", bd, servidor, usuario, pass);
+                    conexion.Open();
+                }
+                else
+                {
+                    conexion.Close();
+                    conexion.ConnectionString = string.Format("Database={0};Server={1};Uid={2};Pwd={3};SslMode=none", bd, servidor, usuario, pass);
+                    conexion.Open();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(string.Format("Error: \n{0}", ex.ToString()), "Conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public DataTable consulta(string sql)
+        {
+            DataTable t = new DataTable();
+            conectar();
+            MySqlCommand comando = conexion.CreateCommand();
+            MySqlDataAdapter adptador = new MySqlDataAdapter();
+            try
+            {
+                comando.Connection = conexion;
+                comando.CommandText = sql;
+                adptador.SelectCommand = comando;
+                adptador.Fill(t);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(string.Format("Error: \n{0}", ex.ToString()), "Conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return t;
+        }
+
+        public void IUD(string sql)
+        {
+            conectar();
+            MySqlCommand comando = conexion.CreateCommand();
+            try
+            {
+                comando.Connection = conexion;
+                comando.CommandText = sql;
+                comando.ExecuteNonQuery();
+                MessageBox.Show("EJECUTADO");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(string.Format("Error: \n{0}", ex.ToString()), "Conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
